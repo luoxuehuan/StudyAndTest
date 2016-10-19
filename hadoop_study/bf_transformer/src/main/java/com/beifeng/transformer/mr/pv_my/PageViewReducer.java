c47e8462ef9214db9557681b064eefd419bc32ef
luoxuehuan <18868825313@163.com>
2016-10-19 22:35:40 +0800

    1212
    12

--- hadoop_study/bf_transformer/src/main/java/com/beifeng/transformer/mr/pv_my/PageViewReducer.java
+++ hadoop_study/bf_transformer/src/main/java/com/beifeng/transformer/mr/pv_my/PageViewReducer.java
@@ -1,8 +1,13 @@
 package com.beifeng.transformer.mr.pv_my;
 
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import com.beifeng.common.KpiType;
 import com.beifeng.transformer.model.dim.StatsUserDimension;
 import com.beifeng.transformer.model.value.reduce.MapWritableValue;
 
@@ -13,7 +18,35 @@ import com.beifeng.transformer.model.value.reduce.MapWritableValue;
  */
 public class PageViewReducer extends Reducer<StatsUserDimension,NullWritable,StatsUserDimension,MapWritableValue>{
 	
-	private MapWritabelValue 
+	private MapWritableValue mapWritableValue = new MapWritableValue();
+	
+	private MapWritable map = new MapWritable();
+	
+	protected void reduce (StatsUserDimension key,Iterable<NullWritable> values,Context context) throws IOException, InterruptedException{
+		int pvCount = 0;
+		
+		/**
+		 * 有一个输入统计一次输入 pvCount+1
+		 */
+		for(NullWritable value : values){
+			  pvCount ++;      
+		}
+		
+		/**
+		 * 填充value
+		 */
+		this.map.put(new IntWritable(-1),new IntWritable(pvCount));
+		this.mapWritableValue.setValue(this.map);
+		
+		/**
+		 * 填充kpi
+		 */
+		this.mapWritableValue.setKpi(KpiType.valueOfName(key.getStatsCommon().getKpi().getKpiName()     ));
+		
+		//输出
+		
+		context.write(key, this.mapWritableValue);
+	}
 	
 
 }
