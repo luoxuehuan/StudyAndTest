f24e14876d776963ee18e9f2222baa184d772c5b
罗学焕 <lxuehuan@gmail.com>
2016-10-19 21:58:00 +0800

    32
    23

--- spark2-upgrade/src/main/scala/cn/ibeifeng/spark/StructuredNetworkWordCount.scala
+++ spark2-upgrade/src/main/scala/cn/ibeifeng/spark/StructuredNetworkWordCount.scala
@@ -10,19 +10,31 @@ object StructuredNetworkWordCount {
         .appName("StructuredNetworkWordCount")  
         .getOrCreate()
         
+        
+    val myspark = SparkSession.builder().appName("d").getOrCreate()
     import spark.implicits._
     
     val lines = spark.readStream
-        .format("socket")
-        .option("host", "localhost")  
+        .format("socket")//指定流的格式
+        .option("host", "localhost")  //设定参数
         .option("port", "9999")  
-        .load()
+        .load()//load得到输入流
+        
+        
+    /**
+     * word是一个dataset
+     */
     val words = lines.as[String].flatMap(_.split(" "))  
+    
+    
+    /**
+     * wordCount是一个dataframe
+     */
     val wordCounts = words.groupBy("value").count() 
     
     val query = wordCounts.writeStream
-        .outputMode("complete") 
-        .format("console")
+        .outputMode("complete") //所有的
+        .format("console")//输出到命令行
         .start()
         
     query.awaitTermination()  
